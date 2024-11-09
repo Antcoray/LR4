d50a45cb71b3dd5efbab2a8ab620bdd061fd63ac
anton <antonio.kolesnikow@gmail.com>
2024-11-09 15:37:55 +0300

    Task_1 done with gtest

--- Task_1/.Testing/Task_1_TESTS.cpp
+++ Task_1/.Testing/Task_1_TESTS.cpp
@@ -1,69 +1,38 @@
 #include <gtest/gtest.h>
 
-#include <sstream>
-
 #include "Task_1.h"
-#define TESTINGinPROGRESS
-
-// Тестирование функции correctInputk
-TEST(InputTest, CorrectInputK) {
-  std::istringstream input("5\n");
-  std::cin.rdbuf(input.rdbuf());  // Перенаправляем ввод
-  EXPECT_EQ(correctInputk(), 5);
-}
-
-TEST(InputTest, IncorrectInputK) {
-  std::istringstream input("abc\n5\n");
-  std::cin.rdbuf(input.rdbuf());  // Перенаправляем ввод
-  EXPECT_EQ(correctInputk(),
-            5);  // Должен игнорировать некорректный ввод и принять 5
-}
-
-// Тестирование функции correctInputx
-TEST(InputTest, CorrectInputX) {
-  std::istringstream input("3.5\n");
-  std::cin.rdbuf(input.rdbuf());  // Перенаправляем ввод
-  EXPECT_EQ(correctInputx(0), 3.5);
-}
-
-TEST(InputTest, IncorrectInputX) {
-  std::istringstream input("abc\n3.5\n");
-  std::cin.rdbuf(input.rdbuf());  // Перенаправляем ввод
-  EXPECT_EQ(correctInputx(0),
-            3.5);  // Должен игнорировать некорректный ввод и принять 3.5
-}
-
-// Тестирование функции findInversions
-TEST(InversionTest, NoInversions) {
-  double array[] = {1, 2, 3, 4, 5};
-  EXPECT_EQ(findInversions(array, 4), 0);  // Нет инверсий
-}
-
-TEST(InversionTest, SomeInversions) {
-  double array[] = {5, 3, 2, 4, 1};
-  EXPECT_EQ(findInversions(array, 4), 8);  // 8 инверсий
-}
 
 TEST(InversionTest, AllInversions) {
   double array[] = {5, 4, 3, 2, 1};
-  EXPECT_EQ(findInversions(array, 4), 10);  // 10 инверсий
+  ASSERT_EQ(findInversions(array, 4), 10);
 }
 
-// Тестирование функции OutputInput
-TEST(OutputTest, ArrayOutput) {
-  std::ostringstream output;
-  std::streambuf* coutbuf = std::cout.rdbuf();
-  std::cout.rdbuf(output.rdbuf());  // Перенаправляем вывод
-
-  double array[] = {1, 2, 3};
-  OutputInput(array, 2);
-
-  std::cout.rdbuf(coutbuf);  // Восстанавливаем стандартный вывод
-
-  EXPECT_EQ(output.str(), "Ваш массив выглядит так: \n[1] [2] [3] \n");
+TEST(InversionTest, ReverseInversions) {
+  double array[] = {1, 2, 3, 4, 5};
+  ASSERT_EQ(findInversions(array, 4), 0);
 }
 
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
+TEST(InversionTest, SymmetricalNumInversions) {
+  double array[] = {1, 2, 3, 2, 1};
+  ASSERT_EQ(findInversions(array, 4), 4);
+}
+
+TEST(InversionTest, NotIntInversions) {
+  double array[] = {5.5, 4.4, 3.3, 2.2, 1.1};
+  ASSERT_EQ(findInversions(array, 4), 10);
+}
+
+TEST(InversionTest, ArrayOfOneNumber) {
+  double array[] = {5.99};
+  ASSERT_EQ(findInversions(array, 0), 0);
+}
+
+TEST(InversionTest, ArrayOfSimilarNumbers) {
+  double array[] = {5.99, 5.99, 5.99, 5.99, 5.99};
+  ASSERT_EQ(findInversions(array, 4), 0);
+}
+
+int main() {
+  testing::InitGoogleTest();
   return RUN_ALL_TESTS();
 }
